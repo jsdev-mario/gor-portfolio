3c7faa2460f1dd05d3989668e8d776a74e6de3d3
LucasZhu <lucaszhu127@gmail.com>
2025-08-30 10:14:45 +0800

    init commit

---
+++ js/config.js
@@ -0,0 +1,21 @@
+/******************************************************************************************************************/
+/* BRAX | Responsive Personal Portfolio Template                                                                  */
+/* (c) Rene Puschinger | https://themeforest.net/user/themegrids                                                  */
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["I am full stack developer."];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Enable typed effect  */
+var config_typed = true;
+
+/* Google map parameters */
+var config_mapLat = 51.509036; // latitude
+var config_mapLng = -0.123280; // longitude
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'My company' // marker title
